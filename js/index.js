b7f3cc7130ac6d2232e873cba60a8f97a3cc8b68
meteoratlas <kellyaccnts@gmail.com>
2020-08-27 13:05:08 -0600

    create deck of card sprites

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 const app = new PIXI.Application({
-    width: 640,
-    height: 360,
+    width: 640 * 2,
+    height: 640 * 2,
     backgroundColor: 0x40a940,
     resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
 
 PIXI.Loader.shared.add("assets/spritesheet.json").load(setup);
+
 let sheet;
+
 function setup() {
     sheet = PIXI.Loader.shared.resources["assets/spritesheet.json"].spritesheet;
 }
 
 PIXI.Loader.shared.onComplete.add(function (loader, resources) {
-    let sprite = new Card("Clubs", "10", sheet.textures["10C.png"]);
-    app.stage.addChild(sprite);
+    // let sprite = new Card("Clubs", "10", sheet.textures["10C.png"]);
+    // app.stage.addChild(sprite);
+    initDeck();
 });
 
 PIXI.Loader.shared.load();
@@ -35,6 +38,20 @@ const RANKS = [
     "King",
     "Ace",
 ];
+
+function initDeck() {
+    const deck = [];
+
+    SUITES.forEach((suite) => {
+        RANKS.forEach((rank) => {
+            const abbrRank = rank.length > 2 ? rank[0] : rank;
+            const imageKey = abbrRank + suite[0] + ".png";
+            deck.push(new Card(suite, rank, sheet.textures[imageKey]));
+        });
+    });
+    shuffle(deck);
+    return deck;
+}
 
 function shuffle(array) {
     let currentIndex = array.length,
