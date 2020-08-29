95f47a28dffac43f033b174434a5fa2d9278768a
meteoratlas <kellyaccnts@gmail.com>
2020-08-28 20:45:53 -0600

    add animation library

--- js/index.js
+++ js/index.js
@@ -58,13 +58,10 @@ function shuffle(array) {
         temporaryValue,
         randomIndex;
 
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
